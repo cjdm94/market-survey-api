8a3b514bb13962449b04598420f97f69208ff0e3
Callum Messiter <cmessiter@hotmail.co.uk>
2017-04-08 17:59:12 +0100

    Render the view in response to requests to index page

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/surveys', (req, res, next) => {
-    res.send('Here are some surveys matching your search criteria.');
+  res.send('Here are some surveys matching your search criteria.');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app  = express();
 const api  = require('./routes/api');
@@ -12,6 +13,13 @@ app.use('/api', api);
 // Parses the body of an incoming request as JSON, and exposes it on req.body - easy to interface with
 app.use(bodyParser.json());
 
+// Set the 'public' directory as the home of the application's static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(port, () => {
-    console.log('Server listening on port '+port);
+	console.log('Server listening on port '+port);
 });
+
+app.get('/', (res, req) => {
+	res.render('index');
+})
